037c54cfcee8e520fd65db39968dc5300bfc42e4
bhagirathgalathiya <59164325+bhagirathgalathiya@users.noreply.github.com>
2020-10-15 11:37:35 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,9 +1,4 @@
 <?php
     
-    $dbhost = "localhost";
-    $dbunm = "id12161614_iotworkshop";
-    $dbpass = "iotworkshop";
-    $dbname = "id12161614_iotworkshop";
-    $conn = new mysqli();
-    $conn = mysqli_connect($dbhost,$dbunm,$dbpass,$dbname);
+   #code for connection
 ?>
